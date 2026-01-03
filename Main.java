1e8b827953eb53fb4dd2acc0a25d1a2d0895fc3f
rajkumara13 <rajkumararikrishnan13@gmail.com>
2026-01-03 10:50:18 +0530

    add ParkingSysetm leetcodeno:1603

--- Main.java
+++ Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class ParkingSystem {
+    private int big;
+    private int med;
+    private int small;
+    ParkingSystem(int big, int medium, int small) {
+        this.big=big;
+        this.med=medium;
+        this.small=small;
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public boolean addCar(int carType) {
+        if(carType==1){
+            if(big>0){
+                big--;
+                return true;
+            }
+            return false;
+        }
+        else if(carType==2){
+            if(med>0){
+                med--;
+                return true;
+            }
+            return false;
+        }
+        else{
+            if(small>0){
+                small--;
+                return true;
+            }
+            return false;
         }
     }
 }
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
+public class Main {
+    public static void main(String[] args) {
+          ParkingSystem parkingSystem=new ParkingSystem(1,1,0);
+          parkingSystem.addCar(1);
+          System.out.println(parkingSystem.addCar(2));
+        }
+    }
